fb3eaacbdde31fdd4624df9b6f17e00a94c432c8
mustafa0610 <47644650+mustafa0610@users.noreply.github.com>
2019-04-13 17:10:15 +0200

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Blog, comment, Report, PostImages
+from .models import Blog, comment, Report, PostImages, question
 
 admin.site.register(Blog)
 admin.site.register(comment)
 admin.site.register(Report)
 admin.site.register(PostImages)
+admin.site.register(question)
